b3b6d0da7ae0dedc3c7e95b6dd79ec7cd02763d1
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-03 21:46:04 +0300

    env vars for PORT and for MONGODB_URL

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+// mongoose.connection
+//     .once('openUri', () => console.log('Connected to MongoLab instance.'))
+//     .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(bodyParser.json());
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
