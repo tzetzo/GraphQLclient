40d897549a53260c02e875f11cf38ceddd742d08
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-04 18:19:16 +0300

    navigation added

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -21,7 +21,7 @@ class SongList extends Component {
     return this.props.data.songs.map(({ id, title }) => {
       return (
         <li key={id} className="collection-item">
-          {title}{" "}
+          <Link to={`/songs/${id}`}>{title} </Link>
           <i
             className="material-icons right"
             onClick={() => this.onSongDelete(id)}
