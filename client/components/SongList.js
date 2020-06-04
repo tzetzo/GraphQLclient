f64540bf315f0858485b33dbc257670cd7662200
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-04 09:47:56 +0300

    execute GraphQL query from inside a React Component & access the returned data with "this.props.data"

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
+import { graphql } from "react-apollo"; // the glue b/n React & GraphQL wworld
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return <li key={song.title}>{song.title}</li>;
+    });
+  }
+
   render() {
-    return <div>SongList</div>;
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+
+    return <ul>{this.renderSongs()}</ul>;
   }
 }
 
-//template string is used for the queries:
+//template string is used to construct the query:
 const query = gql`
   {
     songs {
@@ -16,4 +27,5 @@ const query = gql`
   }
 `;
 
-export default SongList;
+//executes the query and places the songs under this.props.data.songs
+export default graphql(query)(SongList);
